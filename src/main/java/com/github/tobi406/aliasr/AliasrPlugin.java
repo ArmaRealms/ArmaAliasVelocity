01b959b5e3d26dc6894b85eeb70d186e23e8306f
SrBedrock <51332006+SrBedrock@users.noreply.github.com>
2026-03-24 18:37:36 -0300

    Modernize Gradle wrappers and plugin code
    Switch build repository to PaperMC and bump dependencies (adventure-api -> 4.26.1, velocity-api -> 3.5.0-SNAPSHOT). Replace and modernize gradlew and gradlew.bat with POSIX-compliant and more robust startup scripts (SPDX header, improved symlink and path handling, argument parsing, JAVA detection, ulimit handling, use -jar gradle-wrapper.jar, improved exit codes). Refactor AliasrPlugin: add imports and @NullMarked, use @Inject from Guice, make many parameters/fields final, remove unused server field and use commandManager for command execution, tighten config loading (finals, resource copy), and improve logging formatting and null-safety. Overall style, safety and compatibility improvements across build scripts and plugin source.

--- src/main/java/com/github/tobi406/aliasr/AliasrPlugin.java
+++ src/main/java/com/github/tobi406/aliasr/AliasrPlugin.java
@@ -1,5 +1,6 @@
 package com.github.tobi406.aliasr;
 
+import com.google.inject.Inject;
 import com.moandjiezana.toml.Toml;
 import com.velocitypowered.api.command.CommandManager;
 import com.velocitypowered.api.command.CommandMeta;
@@ -7,10 +8,9 @@ import com.velocitypowered.api.command.SimpleCommand;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
-
+import org.jspecify.annotations.NullMarked;
 import org.slf4j.Logger;
 
-import javax.inject.Inject;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -25,12 +25,12 @@ import java.util.List;
         name = "Aliasr",
         version = "1.1-BETA",
         description = "Simple Alias manager using RegEx",
-        authors = { "Tobi406" }
+        authors = {"Tobi406"}
 )
+@NullMarked
 public class AliasrPlugin {
     private static AliasrPlugin instance;
 
-    private final ProxyServer server;
     private final CommandManager commandManager;
     private Logger logger;
     private Toml config;
@@ -40,38 +40,36 @@ public class AliasrPlugin {
     List<String> registeredCommands = new ArrayList<>();
 
     @Inject
-    public AliasrPlugin(ProxyServer server, Logger logger, @DataDirectory final Path folder) {
-        this.server = server;
+    public AliasrPlugin(final ProxyServer server, final Logger logger, @DataDirectory final Path folder) {
         this.commandManager = server.getCommandManager();
         this.logger = logger;
         this.folder = folder;
-
         this.config = this.loadConfig(folder);
 
         this.registerCommands();
 
-
         instance = this;
     }
+
     public static AliasrPlugin getInstance() {
         return instance;
     }
 
-    private Toml loadConfig(Path path) {
-        File folder =  path.toFile();
-        File file = new File(folder, "config.toml");
+    private Toml loadConfig(final Path path) {
+        final File folder = path.toFile();
+        final File file = new File(folder, "config.toml");
         if (!file.getParentFile().exists()) {
             file.getParentFile().mkdirs();
         }
 
         if (!file.exists()) {
-            try (InputStream input = getClass().getResourceAsStream("/" + file.getName())) {
+            try (final InputStream input = getClass().getResourceAsStream("/" + file.getName())) {
                 if (input != null) {
                     Files.copy(input, file.toPath());
                 } else {
                     file.createNewFile();
                 }
-            } catch (IOException exception) {
+            } catch (final IOException exception) {
                 exception.printStackTrace();
                 return null;
             }
@@ -88,34 +86,34 @@ public class AliasrPlugin {
     }
 
     public void registerCommands() {
-        CommandMeta aliasrMeta = this.commandManager.metaBuilder("aliasr").plugin(this).build();
+        final CommandMeta aliasrMeta = this.commandManager.metaBuilder("aliasr").plugin(this).build();
         this.commandManager.register(aliasrMeta, new AliasrCommand());
         this.registeredCommands.add("aliasr");
         this.logger.info("Registered plugin command \"aliasr\"");
 
-        List<HashMap<String, String>> aliases = this.config.getList("aliases");
+        final List<HashMap<String, String>> aliases = this.config.getList("aliases");
 
         aliases.forEach(hashMap -> {
-            CommandMeta meta = this.commandManager.metaBuilder(hashMap.get("name")).plugin(this).build();
+            final CommandMeta meta = this.commandManager.metaBuilder(hashMap.get("name")).plugin(this).build();
             this.commandManager.register(meta, new SimpleCommand() {
-                private String args = hashMap.get("args");
-                private String command = hashMap.get("command");
-                private String commandArgs = hashMap.get("commandArgs");
+                private final String args = hashMap.get("args");
+                private final String command = hashMap.get("command");
+                private final String commandArgs = hashMap.get("commandArgs");
 
                 @Override
-                public void execute(Invocation invocation) {
-                    String joinedArgs = String.join(" ", invocation.arguments());
+                public void execute(final Invocation invocation) {
+                    final String joinedArgs = String.join(" ", invocation.arguments());
 
-                    AliasrPlugin.getInstance().server.getCommandManager().executeAsync(
-                        invocation.source(),
-                        this.command
-                            + (args.length() > 0 ? (" " + joinedArgs.replaceAll(this.args, this.commandArgs)) : "")
+                    AliasrPlugin.getInstance().commandManager.executeAsync(
+                            invocation.source(),
+                            this.command
+                                    + (!args.isEmpty() ? (" " + joinedArgs.replaceAll(this.args, this.commandArgs)) : "")
                     );
                 }
             });
 
             this.registeredCommands.add(hashMap.get("name"));
-            this.logger.info("Registered alias command \"" + hashMap.get("name") + "\"");
+            this.logger.info("Registered alias command \"{}\"", hashMap.get("name"));
         });
     }
 
